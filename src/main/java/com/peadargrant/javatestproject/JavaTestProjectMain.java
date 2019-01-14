a4d02b063a5a7729f2408befbbf2542b32e62b4f
Peadar Grant <peadargrant@gmail.com>
2019-01-14 12:17:55 +0000

    initial commit

---
+++ src/main/java/com/peadargrant/javatestproject/JavaTestProjectMain.java
@@ -0,0 +1,17 @@
+/*
+
+This is a simple dummy project for students to verify that their installation
+of NetBeans (or other IDE) is working and supports Maven Java projects.
+
+*/
+
+package com.peadargrant.javatestproject;
+
+public class JavaTestProjectMain {
+    
+    public static void main(String args[]) {
+        System.out.println("Everything working OK if this message prints.");
+    }
+    
+}
+
